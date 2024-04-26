127ba32b6e4166d0774bb91c612b571e8fdacb96
Jeomyo <162671161+Jeomyo@users.noreply.github.com>
2024-04-26 21:56:53 +0900

    Update date_time_calculate.m
    종원님의 아이디어 참고하여 끝까지 구현하였습니다 !! 작동해보시고 혹시 오류 있나 체크해주세요

--- date_time_calculate.m
+++ date_time_calculate.m
@@ -9,7 +9,7 @@ function date_time = date_time_calculate (days)
        days(1,4) = mod(days(1,4), 24);
        days(1,3) = days(1,3) + k;
    end
-   
+
    while 1
    if (days(1,2)== 1 || days(1,2)== 3 || days(1,2)== 5 || days(1,2)== 7 || days(1,2)== 8 || days(1,2)== 10 || days(1,2)== 12)
        if days(1,3) > 31
@@ -18,3 +18,38 @@ function date_time = date_time_calculate (days)
        else
            break;
        end
+
+   elseif (days(1,2)== 4 || days(1,2)== 6 || days(1,2)== 9 || days(1,2)== 11)
+       if days(1,3) > 30
+           days(1,3) = days(1,3) - 30;
+           days(1,2) = days(1,2) + 1;
+       else
+           break;
+       end
+       
+   elseif (days(1,2) == 2)
+       if days(1,3) > 28
+          days(1,3) = days(1,3) - 28;
+          days(1,2) = days(1,2) + 1;
+       else
+           break;
+       end
+
+   else 
+       break;
+   end
+   
+   if days(1,2) > 12
+       y = floor(days(1,2) / 12);
+       days(1,2) = mod(days(1,2), 12);
+       days(1,1) = days(1,1) + y;
+   end
+
+   end
+
+   
+
+   date_time = days;
+
+
+end
