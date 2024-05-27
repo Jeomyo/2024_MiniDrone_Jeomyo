1a7fb74b27645db96b672083674d1ac74d55cf1f
Jeomyo <162671161+Jeomyo@users.noreply.github.com>
2024-05-27 13:56:09 +0900

    Create main.m

---
+++ 2차과제/main.m
@@ -0,0 +1,41 @@
+droneobj = ryze("TELLO-FE01F2");
+
+cameraobj = camera(droneobj);
+preview(cameraobj);
+
+takeoff(droneobj);
+pause(1);
+
+moveleft(droneobj,'Distance', 1);
+pause(1);
+
+turn(droneobj,deg2rad(360));
+pause(1);
+
+moveforward(droneobj, 'Distance', 1);
+pause(1);
+
+turn(droneobj,deg2rad(90));
+pause(1);
+
+moveforward(droneobj, 'Distance', 1);
+pause(1);
+
+frame = snapshot(cameraobj);
+imshow(frame);
+
+turn(droneobj,deg2rad(360));
+pause(1);
+
+moveright(droneobj,'Distance', 1);
+pause(1);
+
+land(droneobj);
+
+clear all;
+
+
+
+
+
+
